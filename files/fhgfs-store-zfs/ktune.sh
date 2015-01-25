80d73cbee56fd2b48446e01c6396777678526d38
Trey Dockendorf <treydock@gmail.com>
2015-01-24 22:02:45 -0600

    * Dial back tuned profile for fhgfs-store-zfs

--- files/fhgfs-store-zfs/ktune.sh
+++ files/fhgfs-store-zfs/ktune.sh
@@ -6,14 +6,16 @@ start() {
 	set_cpu_governor performance
 	set_transparent_hugepages never
     # Now give the IO scheduler more flexibility by increasing the number of schedulable requests
-	multiply_disk_readahead 32
-        # To improve throughput for sequential reads, increase the maximum amount of read-ahead data.
-        # The actual amount of read-ahead is adaptive,
-        # so using a high value here won't harm performance for small random access.
-        for i in /sys/block/sd?/queue/nr_requests; 
-        do 
-                echo 4096 > $i; 
-        done
+    # Conservatively set to 512
+    multiply_disk_readahead 4
+    # To improve throughput for sequential reads, increase the maximum amount of read-ahead data.
+    # The actual amount of read-ahead is adaptive,
+    # so using a high value here won't harm performance for small random access.
+    # Conservatively set to 1024
+    for i in /sys/block/sd?/queue/nr_requests;
+    do
+        echo 1024 > $i;
+    done
 
 	return 0
 }
@@ -22,8 +24,8 @@ stop() {
 	restore_cpu_governor
 	restore_transparent_hugepages
 	restore_disk_readahead
-        for i in /sys/block/sd?/queue/nr_requests; 
-        do 
+        for i in /sys/block/sd?/queue/nr_requests;
+        do
                 echo 128 > $i; 
         done
 
